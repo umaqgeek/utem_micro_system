24f9d12fc6bb19a0440e6c765a26683edaa5ed01
rasyidi92 <rasyidi.hamzah@gmail.com>
2015-09-13 22:53:20 +0800

    second test message

--- application/views/syidi_message.php
+++ application/views/syidi_message.php
@@ -14,28 +14,32 @@
 		background-color: #fff;
 		margin: 40px;
 		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #FFF;
+		color:#000;
+		border-color:#F03;
 	}
 	a {
-		color:#FFF;
+		color:#000;
 		background-color: transparent;
 		font-weight: bold;
 	}
 
 	h1 {
+		text-align:center;
 		color:#FFF;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
+		background-color:#F03;
+		border: 1px solid #D0D0D0;
 		font-size: 19px;
 		font-weight: bold;
 		margin: 0 0 14px 0;
 		padding: 14px 15px 10px 15px;
+		border-color:#F03;
 	}
 
 	code {
 		font-family: Consolas, Monaco, Courier New, Courier, monospace;
 		font-size: 12px;
-		background-color:#000;
+		font-weight:bold;
+		background-color:#F66;
 		border: 1px solid #D0D0D0;
 		color: #FFF;
 		display: block;
@@ -54,13 +58,18 @@
 		line-height: 32px;
 		padding: 0 10px 0 10px;
 		margin: 20px 0 0 0;
+		background-color:#F03;
+		color:#FFF;
+		border-color:#F03;
+		
 	}
 	
 	#container{
 		margin: 10px;
 		border: 1px solid #D0D0D0;
 		-webkit-box-shadow: 0 0 8px #D0D0D0;
-		background-color:#F00;
+		border-color:#F03;
+	
 	}
 	</style>
 </head>
