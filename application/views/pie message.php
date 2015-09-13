1ac23dcaade909700987749c240b01247a081bcb
Ameerulhadi <Ameerulhadi93@gmail.com>
2015-09-13 14:37:51 +0800

    tukar color

--- application/views/pie message.php
+++ application/views/pie message.php
@@ -11,7 +11,7 @@
 	::webkit-selection{ background-color: #E13300; color: white; }
 
 	body {
-		background-color: #fff;
+		background-color: #000;
 		margin: 40px;
 		font: 13px/20px normal Helvetica, Arial, sans-serif;
 		color: #f0f;
